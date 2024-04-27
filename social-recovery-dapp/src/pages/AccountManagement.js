e6f23aeb21c26cab47bdf69835977763067d7243
WangNianZu <nianzu@gapp.nthu.edu.tw>
2024-04-28 01:36:05 +0800

    version1-4

--- social-recovery-dapp/src/pages/AccountManagement.js
+++ social-recovery-dapp/src/pages/AccountManagement.js
@@ -13,6 +13,8 @@ const AccountManagement = () => {
     const [data, setData] = useState({});
     const [show, setShow] = useState(false);
     const [show2, setShow2] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
     useEffect(() => {
         const load = async() =>{
             if (isloaded) {
@@ -32,9 +34,12 @@ const AccountManagement = () => {
             setContentVisiable(true);
         }
         load();
-    }, []);
+    }, [isloaded]);
     const handleClick = (e) => {
-        setShow(true)
+        setShow(true);
+    };
+    const handleClick2 = (e) => {
+        setShow2(true);
     };
     const handleChange = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
@@ -42,9 +47,31 @@ const AccountManagement = () => {
     const handleClose = (e) => {
         setShow(false);
         setShow2(false);
+        setIsError(false);
+        setErrorMsg("");
+        setData({});
     };
     const depositMoney = async(e) => {
         e.preventDefault();
+        const web3 = new Web3(Web3.givenProvider || 'http://172.0.0.1:7545');
+        var balance = await web3.eth.getBalance(account);
+        // web3.fromWei()就是把Wei轉成其他單位，下面範例則是轉成Ether。toFixed()是我們只取到小數點後兩位數
+        if(isNaN(data.deposit)){
+            setIsError(true);
+            setErrorMsg("錯誤：不能輸入文字，要輸入數字！！");
+            return;
+        }else{
+            if(balance<data.deposit){
+                setIsError(true);
+                setErrorMsg("錯誤：存入金額已經超過現有 EOA 錢包擁有的金額！！");
+                return;
+            }
+            if(data.deposit==0){
+                setIsError(true);
+                setErrorMsg("錯誤：存入金額不能為0！！");
+                return;
+            }
+        }
         const sendData = {
             deposit: data.deposit
         };
@@ -61,12 +88,28 @@ const AccountManagement = () => {
     };
     const withdrawMoney= async(e) =>{
         e.preventDefault();
+        if(isNaN(data.withdraw)){
+            setIsError(true);
+            setErrorMsg("錯誤：不能輸入文字，要輸入數字！！");
+            return;
+        }else{
+            if(accountBalances<data.withdraw){
+                setIsError(true);
+                setErrorMsg("錯誤：領出金額已經超過 Social Recovery Wallet 裡頭的總金額！！");
+                return;
+            }
+            if(data.withdraw==0){
+                setIsError(true);
+                setErrorMsg("錯誤：領出金額不能為0！！");
+                return;
+            }
+        }
         const sendData = {
             withdraw: data.withdraw
         };
         setIsloading(true);
         setContentVisiable(false);
-        contactList.methods.withdrawMoney(sendData.withdraw).send({ from: account})
+        contactList.methods.withdrawMoney(sendData.withdraw).send({from: account})
             .then(function (receipt) {
                 window.location.reload();
             })
@@ -92,11 +135,14 @@ const AccountManagement = () => {
                             <hr />
                             <br />
                             <p>帳戶擁有者地址: {owner} </p>
-                            <p>帳戶總金額：{accountBalances}</p>
-                            ({owner==account})&&
-                            (<Button size="md" className="btn btn-dark" onClick={handleClick}> 存錢 </Button>
-                            <Button size="md" className="btn btn-dark" onClick={handleClick}> 領錢 </Button>
-                            )
+                            <p>帳戶總金額：{accountBalances} wei</p>
+                            {owner===account&&(
+                                <div>
+                                <Button size="md" className="btn btn-dark" onClick={handleClick}> 存錢 </Button>
+                                <Button size="md" className="btn btn-dark" onClick={handleClick2}> 領錢 </Button>
+                                </div>
+                                )
+                            }
                         </div>
                     )
                 }
@@ -116,7 +162,7 @@ const AccountManagement = () => {
                                 <h5><span styles={{ color: '#d78559' }}>| </span> 存入金額 </h5>
                             </Col>
                             <Col>
-                                <Form.Control className="form-control mb-3" placeholder={`how much money to deposit...`} type="text"
+                                <Form.Control className="form-control mb-3" placeholder={`how much wei to deposit...`} type="text"
                                     onChange={handleChange} name="deposit" value={data.deposit}></Form.Control>
                             </Col>
                         </div>
@@ -126,6 +172,7 @@ const AccountManagement = () => {
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
+                    {(isError)&&(<p style={{color:'red'}}>{errorMsg}</p>)}
                     <Button variant='secondary' onClick={handleClose}> 取消 </Button>
                 </Modal.Footer>
             </Modal>
@@ -144,7 +191,7 @@ const AccountManagement = () => {
                                 <h5><span styles={{ color: '#d78559' }}>| </span> 領出金額 </h5>
                             </Col>
                             <Col>
-                                <Form.Control className="form-control mb-3" placeholder={`how much money to withdraw...`} type="text"
+                                <Form.Control className="form-control mb-3" placeholder={`how much wei to withdraw...`} type="text"
                                     onChange={handleChange} name="withdraw" value={data.withdraw}></Form.Control>
                             </Col>
                         </div>
@@ -154,6 +201,7 @@ const AccountManagement = () => {
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
+                    {(isError)&&(<p style={{color:'red'}}>{errorMsg}</p>)}
                     <Button variant='secondary' onClick={handleClose}> 取消 </Button>
                 </Modal.Footer>
             </Modal>
