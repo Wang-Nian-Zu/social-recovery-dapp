b573facb95aa44e30bd6453751270db5bba91f78
WangNianZu <nianzu@gapp.nthu.edu.tw>
2024-04-29 17:31:18 +0800

    version 3-3

--- social-recovery-dapp/contract/truffle-config.js
+++ social-recovery-dapp/contract/truffle-config.js
@@ -1,5 +1,5 @@
 const { projectId, mnemonic } = require('./secrets.json');
-//const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -10,13 +10,11 @@ module.exports = {
     },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    /*
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
     },
-    */
   },
   compilers: {
     solc: {
