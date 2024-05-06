38bdd109713ffc27579ed6b5ab45d9502daecaaf
WangNianZu <nianzu@gapp.nthu.edu.tw>
2024-05-06 19:22:25 +0800

    version3-6

--- social-recovery-dapp/contract/truffle-config.js
+++ social-recovery-dapp/contract/truffle-config.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { infuraProjectId, mnemonic } = require('./secrets.json');
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        mnemonic, `https://sepolia.infura.io/v3/${infuraProjectId}`
+      ),
+      network_id: 11155111,  // Sepolia's network id
+      gas: 4000000
     }
   },
   compilers: {
